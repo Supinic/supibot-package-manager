cba9d49b28351c15cc10dc648d68cc0209bc825a
Çağla Pickaxe <caglapickaxe@gmail.com>
2020-12-07 18:51:48 +0300

    Use HTTPS to fetch chatters

--- commands/chatneighbour/index.js
+++ commands/chatneighbour/index.js
@@ -20,7 +20,7 @@ module.exports = {
 			};
 		}
 	
-		const { chatters } = await sb.Got(`http://tmi.twitch.tv/group/user/${context.channel.Name}/chatters`).json();
+		const { chatters } = await sb.Got(`https://tmi.twitch.tv/group/user/${context.channel.Name}/chatters`).json();
 		const list = Object.values(chatters).flat();
 
 		if (!list.includes(context.user.Name)) {
