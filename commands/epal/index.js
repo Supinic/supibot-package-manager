3fb91fc7904973daae2cc05a82bd3e29ac640ff4
Sergey Kislyakov <i@defman.me>
2021-01-26 21:36:42 +0300

    Add dot at the end of the revenue string

--- commands/epal/index.js
+++ commands/epal/index.js
@@ -156,7 +156,7 @@ module.exports = {
 			: `$${price.regular} per ${price.unit}`;
 
 		const revenueString = (revenue !== null && revenue > 0)
-			? `Total revenue: $${revenue}`
+			? `Total revenue: $${revenue}.`
 			: "";
 
 		const languageString = (languages.length > 0)
