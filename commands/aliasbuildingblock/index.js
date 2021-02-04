5b7a2d20327a22c393a28f349a925b779c0c7a81
Ben de Graaff <ben@nx3d.org>
2021-02-04 19:13:22 +0100

    aliasbuildingblock: Add missing colon to URLs

--- commands/aliasbuildingblock/index.js
+++ commands/aliasbuildingblock/index.js
@@ -91,7 +91,7 @@ module.exports = {
             if (!type) {
                 return {
                     success: false,
-                    reply: `This command can only be used within aliases! Check help here: https//supinic.com/bot/command/${this.ID}`
+                    reply: `This command can only be used within aliases! Check help here: https://supinic.com/bot/command/${this.ID}`
                 };
             }
 
@@ -100,7 +100,7 @@ module.exports = {
             if (!block) {
                 return {
                     success: false,
-                    reply: `This command can only be used within aliases! Check help here: https//supinic.com/bot/command/${this.ID}`
+                    reply: `This command can only be used within aliases! Check help here: https://supinic.com/bot/command/${this.ID}`
                 };
             }
             else {
@@ -114,7 +114,7 @@ module.exports = {
         if (!type) {
             return {
                 success: false,
-                reply: `No block type provided! Check help here: https//supinic.com/bot/command/${this.ID}`
+                reply: `No block type provided! Check help here: https://supinic.com/bot/command/${this.ID}`
             };
         }
 
@@ -123,7 +123,7 @@ module.exports = {
         if (!block) {
             return {
                 success: false,
-                reply: `Incorrect block type provided! Check help here: https//supinic.com/bot/command/${this.ID}`
+                reply: `Incorrect block type provided! Check help here: https://supinic.com/bot/command/${this.ID}`
             };
         }
 
