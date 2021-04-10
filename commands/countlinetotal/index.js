deef9a11d0f86eef884c34ec2c8ae73386fed193
Quinn T <Quinn.GitHub@gmail.com>
2021-04-10 18:15:32 -0500

    fix access before initialzation

--- commands/countlinetotal/index.js
+++ commands/countlinetotal/index.js
@@ -31,10 +31,10 @@ module.exports = {
 		const currentSize = sb.Utils.round(data.Bytes / (10 ** 9), 3);
 		
 		if(history) {
-			const megabytesPerHour = sb.Utils.round(rate * 1024 / 24, 3);
 			const days = (sb.Date.now() - history.Executed) / 864.0e5;
 			const originalSize = Number(history.Result.match(/([\d.]+) GB of space/)[1]);
 			const rate = sb.Utils.round((currentSize - originalSize) / days, 3);
+			const megabytesPerHour = sb.Utils.round(rate * 1024 / 24, 3);
 			const fillDate = new sb.Date().addDays((220 - currentSize) / rate); // 238 GB minus an estimate of ~18GB of other stuff
 			historyText = sb.Utils.tag.trim `
 					Lines are added at a rate of ~${megabytesPerHour} MB/hr.
